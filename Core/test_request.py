54b8ff37004220b5c0c94e6a855eba6f5a14ddc5
Maxim <makoli744@gmail.com>
2025-06-24 16:49:13 +0300

    ver 1.2.6

--- Core/test_request.py
+++ Core/test_request.py
@@ -3,34 +3,39 @@ import requests
 
 url = "http://192.168.1.3:80"
 
-def resp(url, endpoint):
+def resp_get(url, endpoint):
     response = requests.get(f"{url}{endpoint}")
     response.raise_for_status()
     data = response.json()
     return data
 
-def resp_params(url, endpoint, params):
+def resp_params_get(url, endpoint, params):
     response = requests.get(f"{url}{endpoint}", params=params)
     response.raise_for_status()
     data = response.json()
     return data 
 
+def resp_post(url, endpoint):
+    response = requests.post(f"{url}{endpoint}")
+    response.raise_for_status()
+    data = response.json()
+    return data
 
 def get_info(url):
     endpoint = "/printer/info"
-    return resp(url, endpoint)
+    return resp_get(url, endpoint)
 
 def pause_print(url):
     endpoint = "/printer/print/pause"
-    return resp(url, endpoint)
+    return resp_post(url, endpoint)
 
 def resume_print(url):
     endpoint = "/printer/print/resume"
-    return resp(url, endpoint)
+    return resp_post(url, endpoint)
 
 def cancle_print(url):
     endpoint = "/printer/print/cancle"
-    return resp(url, endpoint)
+    return resp_post(url, endpoint)
 
 def extruder_info(url):
     endpoint = "/printer/objects/query"
@@ -40,4 +45,4 @@ def extruder_info(url):
         "toolhead": ["temperature", "target"]
         }
     }
-    return resp_params(url, endpoint, params)
+    return resp_params_get(url, endpoint, params)
